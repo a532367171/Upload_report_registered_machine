e0d68f75cd6596817b46b11c35007a1c3988e663
a532367171 <532367171@qq.com>
2019-07-04 18:05:11 +0800

    添加项目文件。

---
+++ Upload_report_registered_machine/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Upload_report_registered_machine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 246);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(18, 18);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 210);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(357, 21);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "大幅度发到付对方答复";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(18, 303);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(758, 21);
+            this.textBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Upload_report_registered_machine/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Upload_report_registered_machine
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string  S= monthCalendar1.SelectionStart.ToString("yyyy-MM-dd")+";"+"3" + ";"+ textBox1.Text+";" + "2";
+            textBox2.Text = DES.Encrypt(DES.Encrypt(S, "z>{6-Ke*"), "R-@.3Ci2");
+        }
+    }
+
+    public class DES
+    {
+        private const string KEY64 = "i?>.Oh-9";
+
+        private const string IV_64 = "i?>.Oh-9";
+
+        public static string Encrypt(string data)
+        {
+            return DES.Encrypt(data, "i?>.Oh-9");
+        }
+
+        public static string Decrypt(string data)
+        {
+            return DES.Decrypt(data, "i?>.Oh-9");
+        }
+
+        public static string Encrypt(string data, string seckey)
+        {
+            byte[] byKey = System.Text.Encoding.ASCII.GetBytes(seckey);
+            byte[] by_IV = System.Text.Encoding.ASCII.GetBytes(seckey);
+            System.Security.Cryptography.DESCryptoServiceProvider cryptoProvider = new System.Security.Cryptography.DESCryptoServiceProvider();
+            int i = cryptoProvider.KeySize;
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+            System.Security.Cryptography.CryptoStream cst = new System.Security.Cryptography.CryptoStream(ms, cryptoProvider.CreateEncryptor(byKey, by_IV), System.Security.Cryptography.CryptoStreamMode.Write);
+            System.IO.StreamWriter sw = new System.IO.StreamWriter(cst);
+            sw.Write(data);
+            sw.Flush();
+            cst.FlushFinalBlock();
+            sw.Flush();
+            return System.Convert.ToBase64String(ms.GetBuffer(), 0, (int)ms.Length);
+        }
+
+        public static string Decrypt(string data, string seckey)
+        {
+            byte[] byKey = System.Text.Encoding.ASCII.GetBytes(seckey);
+            byte[] by_IV = System.Text.Encoding.ASCII.GetBytes(seckey);
+            byte[] byEnc;
+            string result;
+            try
+            {
+                byEnc = System.Convert.FromBase64String(data);
+            }
+            catch
+            {
+                result = null;
+                return result;
+            }
+            System.Security.Cryptography.DESCryptoServiceProvider cryptoProvider = new System.Security.Cryptography.DESCryptoServiceProvider();
+            System.IO.MemoryStream ms = new System.IO.MemoryStream(byEnc);
+            System.Security.Cryptography.CryptoStream cst = new System.Security.Cryptography.CryptoStream(ms, cryptoProvider.CreateDecryptor(byKey, by_IV), System.Security.Cryptography.CryptoStreamMode.Read);
+            System.IO.StreamReader sr = new System.IO.StreamReader(cst);
+            result = sr.ReadToEnd();
+            return result;
+        }
+    }
+}
