1f7ed29fedd4c84bd658b2168ef6fa98d02006d7
a532367171 <532367171@qq.com>
2019-07-04 18:06:29 +0800

    19070401

--- Upload_report_registered_machine/Program.cs
+++ Upload_report_registered_machine/Program.cs
@@ -16,6 +16,7 @@ namespace Upload_report_registered_machine
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
